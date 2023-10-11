78d49c12c4e4f93646fbf08eb3b67344c4aab9ed
ivothexclusive <iverthomas677@gmail.com>
2023-10-11 16:57:24 -0500

    missing pass present class

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,59 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var currentDay = dayjs().format('dddd, MMMM D') 
+$('header').append(currentDay)
+
+//
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(".saveBtn").click(function() {
+  console.log("A button clicked!");
+
+  // Get the value to save from the sibling textarea element
+  var valueToSave = $(this).siblings("textarea").val();
+
+  // Get the key from the parent element's ID
+  var key = $(this).parent().attr('id'); 
+
+  console.log('valueToSave: ', valueToSave);
+  console.log('key: ', key);
+
+  // Save the value to local storage as a JSON string
+  localStorage.setItem(key, JSON.stringify(valueToSave));
 });
+})
+var timeBlock = $(".time-block");
+
+// To load the saved data
+timeBlock.each(function() {
+  // Get the key from the time-block's ID
+  var key = $(this).attr('id');
+
+  // Retrieve the saved data from local storage
+  var retrievedObject = JSON.parse(localStorage.getItem(key));
+
+  // Check if data was retrieved
+  if (retrievedObject !== null) {
+    // Set the value of the textarea in the current time-block
+    $(this).find("textarea").val(retrievedObject);
+  }
+});
+
+$(function () {
+  var currentHour = dayjs().hour()
+
+ $(".time-block").each(function() {
+   
+   const timeBlockId = this.id;
+  const timeBlockHour = parseInt(timeBlockId.split("-")[1]);
+
+  if (timeBlockHour < currentHour) {
+//     // Time block is in the past
+   $(this).addClass("past");
+  } else if (timeBlockHour == currentHour) {
+//     // Time block is in the present
+    $(this).addClass("present");
+ } else {
+//     // Time block is in the future
+    $(this).addClass("future");
+  }
+});
+
+})
