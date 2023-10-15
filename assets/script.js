c1a0d4bc2f5379bfa9b14ce6633e8289d9d2aefe
ivothexclusive <iverthomas677@gmail.com>
2023-10-15 18:22:23 -0500

    update reademe

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,35 @@
-var currentDay = dayjs().format('dddd, MMMM D') 
-$('header').append(currentDay)
-
-//
-$(function () {
-$(".saveBtn").click(function() {
-  console.log("A button clicked!");
-
-  // Get the value to save from the sibling textarea element
-  var valueToSave = $(this).siblings("textarea").val();
-
-  // Get the key from the parent element's ID
-  var key = $(this).parent().attr('id'); 
-
-  console.log('valueToSave: ', valueToSave);
-  console.log('key: ', key);
-
-  // Save the value to local storage as a JSON string
-  localStorage.setItem(key, JSON.stringify(valueToSave));
-});
-})
-var timeBlock = $(".time-block");
-
-// To load the saved data
-timeBlock.each(function() {
-  // Get the key from the time-block's ID
-  var key = $(this).attr('id');
-
-  // Retrieve the saved data from local storage
-  var retrievedObject = JSON.parse(localStorage.getItem(key));
-
-  // Check if data was retrieved
-  if (retrievedObject !== null) {
-    // Set the value of the textarea in the current time-block
-    $(this).find("textarea").val(retrievedObject);
-  }
-});
+var currentDay = dayjs().format('dddd, MMMM D');
+$('header').append(currentDay);
 
 $(function () {
-  var currentHour = dayjs().hour()
+  $(".saveBtn").click(function() {
+    console.log("A button clicked!");
+    var valueToSave = $(this).siblings("textarea").val();
+    var key = $(this).parent().attr('id');
+    console.log('valueToSave: ', valueToSave);
+    console.log('key: ', key);
+    localStorage.setItem(key, valueToSave);
+  });
 
- $(".time-block").each(function() {
-   
-   const timeBlockId = this.id;
-  const timeBlockHour = parseInt(timeBlockId.split("-")[1]);
+  var timeBlock = $(".time-block");
+  timeBlock.each(function() {
+    var key = $(this).attr('id');
+    var retrievedObject = localStorage.getItem(key);
+    if (retrievedObject !== null) {
+      $(this).find("textarea").val(retrievedObject);
+    }
+  });
 
-  if (timeBlockHour < currentHour) {
-//     // Time block is in the past
-   $(this).addClass("past");
-  } else if (timeBlockHour == currentHour) {
-//     // Time block is in the present
-    $(this).addClass("present");
- } else {
-//     // Time block is in the future
-    $(this).addClass("future");
-  }
+  var currentHour = dayjs().hour();
+  timeBlock.each(function() {
+    var timeBlockId = $(this).attr('id');
+    var timeBlockHour = parseInt(timeBlockId.split("-")[1]);
+    if (timeBlockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlockHour == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 });
-
-})
